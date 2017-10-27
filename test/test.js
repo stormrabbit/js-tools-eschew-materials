58ff61443e0664c3f9df964ecedbd681fc834456
欧阳夏昱 <ouyangxiayu@m24.cn>
2017-10-27 22:48:24 +0800

    update 1.0.1

--- test/test.js
+++ test/test.js
@@ -6,17 +6,32 @@ const {
 } = require('../index');
 const co = require('co');
 
+co(function* () {
+  try {
+    const readMe = yield fsTools.readFilePlus(__dirname + '/../ReadMe.md');
+    console.log(readMe);
+    const currentPath = process.cwd();
+    yield fsTools.writeFilePlus(`${__dirname}/test.txt`, 'hello, world');
+    yield fsTools.insertFilePlus(`${__dirname}/test.txt`, '\r\nhello kitty', 'world')
 
-co(function * () {
-  // const readMe = yield fsTools.readFilePlus(__dirname + '/../ReadMe.md');
-  // console.log(readMe);
-  // cmdTools.execCommands(['test', 'test2']);
-  // const exeResult = yield fsTools.readFilePlus(readMe);
-  // console.log(logger)
-  logger.setDebugMode(1);
-  logger.m('fuck');
-  // logger.m('test')
-  // console.log(exeResult);
+    console.log('fsTools 运行正常');
+    const test1 = { a: 'a', b: 'b', c: 'c', d: 'd' };
+    console.log('encode==>\n', cryptoTools.urlParamsEncode(test1));
+    console.log('decode==>:\n', cryptoTools.urlParamsDecode(cryptoTools.urlParamsEncode(test1)));
+    console.log('cryptoTools 运行正常');
+    // cmdTools.execCommands(['test', 'test2']);
+    // const exeResult = yield fsTools.readFilePlus(readMe);
+    // console.log(logger)
+    logger.setDebugMode(1);
+    logger.m('hello, world');
+    console.log('logger 运行正常')
+    // logger.m('test')
+    // console.log(exeResult);
+  } catch (error) {
+    console.log('运行错误：', error);
+  } finally {
+    process.exit()
+  }
 })
 
 // const test1 = {a: 'a', b: 'b', c:'c', d:'d'};
