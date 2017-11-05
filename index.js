dfe589e95ba860c87772d67e922e6273371fb40f
欧阳夏昱 <ouyangxiayu@m24.cn>
2017-11-06 00:05:38 +0800

    fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fsTools = require('./lib/fsTools');
 const cryptoTools = require('./lib/cryptoTools');
 const logger = require('./lib/loggerSington');
-const cmdTools = require('./lib/cmdTools');
+// const cmdTools = require('./lib/cmdTools');
 const txtTools = require('./lib/txtTools');
-module.exports = {fsTools, cryptoTools, logger, cmdTools, txtTools};
+module.exports = {fsTools, cryptoTools, logger, txtTools};
